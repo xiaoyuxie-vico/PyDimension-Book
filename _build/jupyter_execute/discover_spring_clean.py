762350a2644a47b89185955b26a2162721bd2ce3
xiaoyu.xie <xiaoyuxie2020@u.northwestern.edu>
2022-12-23 10:24:04 -0600

    update

---
+++ _build/jupyter_execute/discover_spring_clean.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Content with notebooks
+# 
+# You can also create content with Jupyter Notebooks. This means that you can include
+# code blocks and their outputs in your book.
+# 
+# ## Markdown + notebooks
+# 
+# As it is markdown, you can embed images, HTML, etc into your posts!
+
+# In[1]:
+
+
+print('Hello world!')
+
+
+# 
